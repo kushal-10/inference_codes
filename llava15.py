8cf246f0bf114898f2b060e76a5c0618ce39f9f1
kushal-10 <kushalkoshti11@gmail.com>
2024-04-29 12:35:49 +0200

    update image tag

--- llava15.py
+++ llava15.py
@@ -57,7 +57,7 @@ print(generated_text)
 print("NoImage, image, no image reference CASE ############################################################")
 
 prompts_image_2 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:"
-inputs = processor(prompts_image_2, image=[image1], return_tensors="pt").to("cuda")
+inputs = processor(prompts_image_2, images=[image1], return_tensors="pt").to("cuda")
 
 output = model.generate(**inputs, max_new_tokens=200)
 generated_text = processor.batch_decode(output, skip_special_tokens=True)
@@ -67,7 +67,7 @@ print(generated_text)
 print("NoImage, image, no image reference, new image CASE ############################################################")
 
 prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:"
-inputs = processor(prompts_image_3, image=[image1, image2], return_tensors="pt").to("cuda")
+inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
 
 output = model.generate(**inputs, max_new_tokens=200)
 generated_text = processor.batch_decode(output, skip_special_tokens=True)
@@ -77,7 +77,7 @@ print(generated_text)
 print("NoImage, image, no image reference, new image, no reference CASE ############################################################")
 
 prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:Two cats laying on a couch.\nUSER:\nTell me more about the image?\nASSISTANT:"
-inputs = processor(prompts_image_3, image=[image1, image2], return_tensors="pt").to("cuda")
+inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
 
 output = model.generate(**inputs, max_new_tokens=200)
 generated_text = processor.batch_decode(output, skip_special_tokens=True)
