187265814b1753376c39550bb9e322993243335b
kushal-10 <kushalkoshti11@gmail.com>
2024-05-01 08:13:22 +0200

    check idefics

--- context_check.py
+++ context_check.py
@@ -1,15 +1,19 @@
 from transformers import AutoProcessor, AutoModelForVision2Seq, AutoConfig
 
-model_ids = [
-    "llava-hf/llava-1.5-7b-hf",
-    "llava-hf/llava-1.5-13b-hf",
-    # "HuggingFaceM4/idefics-80b-instruct",
-    "llava-hf/llava-v1.6-vicuna-7b-hf",
-    "llava-hf/llava-v1.6-vicuna-13b-hf",
-    "llava-hf/llava-v1.6-mistral-7b-hf",
-    "llava-hf/llava-v1.6-34b-hf"]
+# model_ids = [
+#     "llava-hf/llava-1.5-7b-hf",
+#     "llava-hf/llava-1.5-13b-hf",
+#     "HuggingFaceM4/idefics-80b-instruct",
+#     "llava-hf/llava-v1.6-vicuna-7b-hf",
+#     "llava-hf/llava-v1.6-vicuna-13b-hf",
+#     "llava-hf/llava-v1.6-mistral-7b-hf",
+#     "llava-hf/llava-v1.6-34b-hf"]
 
-for model in model_ids:
-    model_config = AutoConfig.from_pretrained(model)
-    # print(model_config)
-    print(f"Max embeddings for {model}: {model_config.text_config.max_position_embeddings}")
+# for model in model_ids:
+#     model_config = AutoConfig.from_pretrained(model)
+#     # print(model_config)
+#     print(f"Max embeddings for {model}: {model_config.text_config.max_position_embeddings}")
+
+model_id = "HuggingFaceM4/idefics-80b-instruct"
+model_config = AutoConfig.from_pretrained(model)
+print(model_config)
