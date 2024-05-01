ec103fc251fb71f3e48a7bd17e3a4002925459b5
kushal-10 <kushalkoshti11@gmail.com>
2024-05-01 08:12:01 +0200

    check for all models

--- context_check.py
+++ context_check.py
@@ -1,7 +1,15 @@
 from transformers import AutoProcessor, AutoModelForVision2Seq, AutoConfig
 
-model_id = "llava-hf/llava-1.5-7b-hf"
+model_ids = [
+    "llava-hf/llava-1.5-7b-hf",
+    "llava-hf/llava-1.5-13b-hf",
+    "HuggingFaceM4/idefics-80b-instruct",
+    "llava-hf/llava-v1.6-vicuna-7b-hf",
+    "llava-hf/llava-v1.6-vicuna-13b-hf",
+    "llava-hf/llava-v1.6-mistral-7b-hf",
+    "llava-hf/llava-v1.6-34b-hf"]
 
-model_config = AutoConfig.from_pretrained(model_id)
-print(model_config)
-print(model_config.text_config.max_position_embeddings)
+for model in model_ids:
+    model_config = AutoConfig.from_pretrained(model)
+    # print(model_config)
+    print(f"Max embeddings for {model}: {model_config.text_config.max_position_embeddings}")
