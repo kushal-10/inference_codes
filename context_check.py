65256989203d65bb36f19cfbe1c87bf57eb09428
kushal-10 <kushalkoshti11@gmail.com>
2024-05-01 10:58:43 +0200

    tokenize

--- context_check.py
+++ context_check.py
@@ -7,6 +7,8 @@ processor = AutoProcessor.from_pretrained(model_id)
 tokenizer = processor.tokenizer
 
 prompt = "USER: <image> What is shown in this image? \nASSISTANT:"
-tokenized_prompt = tokenizer.apply_chat_template(prompt)
+tokenized_prompt = tokenizer.tokenize(prompt)
+
 
 print(tokenized_prompt)
+print(len(tokenized_prompt))
