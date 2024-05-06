4af887b352b0a059987474ffed917dd1d2022c04
kushal-10 <kushalkoshti11@gmail.com>
2024-05-06 14:57:15 +0200

    test template

--- llavallama.py
+++ llavallama.py
@@ -8,57 +8,38 @@ image2 = Image.open(requests.get("http://images.cocodataset.org/val2017/00000003
 
 
 # model_id = "llava-hf/llava-1.5-13b-hf"
-model_id = "llava-hf/llava-1.5-7b-hf"
+# model_id = "llava-hf/llava-1.5-7b-hf"
 model_id = 'xtuner/llava-llama-3-8b-v1_1-transformers'
 
-processor = AutoProcessor.from_pretrained(model_id, device_map="auto", verbose=False)
-model = AutoModelForVision2Seq.from_pretrained(model_id, device_map="auto", torch_dtype="auto")
+# processor = AutoProcessor.from_pretrained(model_id, device_map="auto", verbose=False)
+# model = AutoModelForVision2Seq.from_pretrained(model_id, device_map="auto", torch_dtype="auto")
+
+# Chat Template:
+'''
+"<|start_header_id|>user<|end_header_id|>\n\n<image>\nWhat are these?<|eot_id|>
+ <|start_header_id|>assistant<|end_header_id|>\n\n"
+'''
+mess = [
+    {'role': 'system', 'content': 'This is a system message'},
+    {'role': 'user', 'content': 'What is shown in the image?', 'image': 'https://llava-vl.github.io/static/images/view.jpg'},
+    {'role': 'assistant', 'content': ' A lake, A deck and A forest.'},
+    {'role': 'user', 'content': 'Explain a bit more about this image'},
+    {'role': 'assistant', 'content': 'The image features a serene scene of a lake with a pier extending out into the water. The pier is made of wood and appears to be a popular spot for relaxation and enjoying the view. The lake is surrounded by a forest, adding to the natural beauty of the area. The overall atmosphere of the image is peaceful and inviting.'},
+    {'role': 'user', 'content': 'What is shown in this image?', 'image': 'http://images.cocodataset.org/val2017/000000039769.jpg'}
+]
 
 
-print("GENERAL CASE ############################################################")
-prompts = "USER: <image>\nWhat is shown in the image?\nASSISTANT:"
-inputs = processor(prompts, images=[image1], return_tensors="pt").to("cuda")
+jinja_template_backup = '''{%- for message in messages -%}{% if message['role'] == 'user' %}{% if message['image'] %}<|start_header_id|>user<|end_header_id|>\n\n<image>\n{{message['content']}}<|eot_id|>{% else %}<|start_header_id|>user<|end_header_id|>\n\n{{message['content']}}<|eot_id|>{% endif %}{% elif message['role'] == 'assistant' %}<|start_header_id|>assistant<|end_header_id|>\n\n{{message['content']}}<|eot_id|>{% endif %}{% endfor %}<|start_header_id|>assistant<|end_header_id|>\n\n'''
 
-output = model.generate(**inputs, max_new_tokens=200)
-generated_text = processor.batch_decode(output, skip_special_tokens=True)
-print(generated_text)
+temp = Template(jinja_template)
+promptu = temp.render(messages=mess)
+print(promptu)
 
-print("NO IMAGE CASE ############################################################")
+# # Passes all cases (with/without/ref images ....)
+# print("GENERAL CASE ############################################################")
+# prompts = "USER: <image>\nWhat is shown in the image?\nASSISTANT:"
+# inputs = processor(prompts, images=[image1], return_tensors="pt").to("cuda")
 
-prompts_wo_image = "USER: What are you?\nASSISTANT:"
-inputs = processor.tokenizer(prompts_wo_image, return_tensors="pt").to("cuda")
-
-output = model.generate(**inputs, max_new_tokens=200)
-generated_text = processor.batch_decode(output, skip_special_tokens=True)
-print(generated_text)
-
-
-print("NoImage, image, no image reference CASE ############################################################")
-
-prompts_image_2 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:"
-inputs = processor(prompts_image_2, images=[image1], return_tensors="pt").to("cuda")
-
-output = model.generate(**inputs, max_new_tokens=200)
-generated_text = processor.batch_decode(output, skip_special_tokens=True)
-print(generated_text)
-
-
-print("NoImage, image, no image reference, new image CASE ############################################################")
-
-prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:"
-inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
-
-output = model.generate(**inputs, max_new_tokens=200)
-generated_text = processor.batch_decode(output, skip_special_tokens=True)
-print(generated_text)
-
-
-print("NoImage, image, no image reference, new image, no reference CASE ############################################################")
-
-prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:Two cats laying on a couch.\nUSER:\nTell me more about the image?\nASSISTANT:"
-inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
-
-output = model.generate(**inputs, max_new_tokens=200)
-generated_text = processor.batch_decode(output, skip_special_tokens=True)
-print(generated_text)
-
+# output = model.generate(**inputs, max_new_tokens=200)
+# generated_text = processor.batch_decode(output, skip_special_tokens=True)
+# print(generated_text)
