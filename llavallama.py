56e0db4cc6e1044dead21fc58104736e5b950f98
kushal-10 <kushalkoshti11@gmail.com>
2024-05-06 14:48:31 +0200

    test all cases

--- llavallama.py
+++ llavallama.py
@@ -19,46 +19,46 @@ print("GENERAL CASE ############################################################
 prompts = "USER: <image>\nWhat is shown in the image?\nASSISTANT:"
 inputs = processor(prompts, images=[image1], return_tensors="pt").to("cuda")
 
-# output = model.generate(**inputs, max_new_tokens=200)
-# generated_text = processor.batch_decode(output, skip_special_tokens=True)
-# print(generated_text)
+output = model.generate(**inputs, max_new_tokens=200)
+generated_text = processor.batch_decode(output, skip_special_tokens=True)
+print(generated_text)
 
-# print("NO IMAGE CASE ############################################################")
+print("NO IMAGE CASE ############################################################")
 
-# prompts_wo_image = "USER: What are you?\nASSISTANT:"
-# inputs = processor.tokenizer(prompts_wo_image, return_tensors="pt").to("cuda")
+prompts_wo_image = "USER: What are you?\nASSISTANT:"
+inputs = processor.tokenizer(prompts_wo_image, return_tensors="pt").to("cuda")
 
-# output = model.generate(**inputs, max_new_tokens=200)
-# generated_text = processor.batch_decode(output, skip_special_tokens=True)
-# print(generated_text)
-
-
-# print("NoImage, image, no image reference CASE ############################################################")
-
-# prompts_image_2 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:"
-# inputs = processor(prompts_image_2, images=[image1], return_tensors="pt").to("cuda")
-
-# output = model.generate(**inputs, max_new_tokens=200)
-# generated_text = processor.batch_decode(output, skip_special_tokens=True)
-# print(generated_text)
+output = model.generate(**inputs, max_new_tokens=200)
+generated_text = processor.batch_decode(output, skip_special_tokens=True)
+print(generated_text)
 
 
-# print("NoImage, image, no image reference, new image CASE ############################################################")
+print("NoImage, image, no image reference CASE ############################################################")
 
-# prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:"
-# inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
+prompts_image_2 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:"
+inputs = processor(prompts_image_2, images=[image1], return_tensors="pt").to("cuda")
 
-# output = model.generate(**inputs, max_new_tokens=200)
-# generated_text = processor.batch_decode(output, skip_special_tokens=True)
-# print(generated_text)
+output = model.generate(**inputs, max_new_tokens=200)
+generated_text = processor.batch_decode(output, skip_special_tokens=True)
+print(generated_text)
 
 
-# print("NoImage, image, no image reference, new image, no reference CASE ############################################################")
+print("NoImage, image, no image reference, new image CASE ############################################################")
 
-# prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:Two cats laying on a couch.\nUSER:\nTell me more about the image?\nASSISTANT:"
-# inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
+prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:"
+inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
 
-# output = model.generate(**inputs, max_new_tokens=200)
-# generated_text = processor.batch_decode(output, skip_special_tokens=True)
-# print(generated_text)
+output = model.generate(**inputs, max_new_tokens=200)
+generated_text = processor.batch_decode(output, skip_special_tokens=True)
+print(generated_text)
+
+
+print("NoImage, image, no image reference, new image, no reference CASE ############################################################")
+
+prompts_image_3 = "USER: What are you?\nASSISTANT:I am Vicuna, a virtual assistant. How can I help you?\nUSER: <image>\nWhat is shown in the image?\nASSISTANT: A lake, A deck and A forest.\nUSER\nTell me more about the image.\nASSISTANT:An amazing scenery providing a glancing view of a port or deck with a lake surrounded by a lush green forest.\nUSER: <image>\nWhat is shown in the image?\nASSISTANT:Two cats laying on a couch.\nUSER:\nTell me more about the image?\nASSISTANT:"
+inputs = processor(prompts_image_3, images=[image1, image2], return_tensors="pt").to("cuda")
+
+output = model.generate(**inputs, max_new_tokens=200)
+generated_text = processor.batch_decode(output, skip_special_tokens=True)
+print(generated_text)
 
