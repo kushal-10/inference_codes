21b3dc5b35ff2344c2ae740ed6ef4322cc169dc3
kushal-10 <kushalkoshti11@gmail.com>
2024-05-09 02:20:42 +0200

    combine images script

---
+++ additional_tests/combine_images.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+image1 = Image.open("https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG")
+image2 = Image.open("https://static.wikia.nocookie.net/asterix/images/2/25/R22b.gif/revision/latest?cb=20110815073052")
+image3 = Image.open("https://llava-vl.github.io/static/images/view.jpg")
+
+# Resize images to have the same height
+min_height = min(image1.height, image2.height, image3.height)
+image1 = image1.resize((int(image1.width * min_height / image1.height), min_height))
+image2 = image2.resize((int(image2.width * min_height / image2.height), min_height))
+image3 = image3.resize((int(image3.width * min_height / image3.height), min_height))
+
+combined_width = image1.width + image2.width + image3.width
+
+# Create a new blank image 
+combined_image = Image.new("RGB", (combined_width, min_height))
+
+# Paste each image
+combined_image.paste(image1, (0, 0))
+combined_image.paste(image2, (image1.width, 0))
+combined_image.paste(image3, (image1.width + image2.width, 0))
+
+combined_image.save("combined_image.jpg")
