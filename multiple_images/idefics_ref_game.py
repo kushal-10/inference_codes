c05516d80a43b78326dea9c0036d88a034e4a57d
kushal-10 <kushalkoshti11@gmail.com>
2024-05-11 19:11:37 +0200

    base test

--- multiple_images/idefics_ref_game.py
+++ multiple_images/idefics_ref_game.py
@@ -3,23 +3,10 @@ from transformers import IdeficsForVisionText2Text, AutoProcessor
 
 from jinja2 import Template
 
-'''
-IMAGES - 
-"https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG" -> Idefics - Running Dog
-"https://static.wikia.nocookie.net/asterix/images/2/25/R22b.gif/revision/latest?cb=20110815073052" -> Asterix - Man with red cape
-"https://llava-vl.github.io/static/images/view.jpg" -> View of a lake surrounded by forest
-'''
-
 prompt1 = [
     [
-        "User: You are given three images, one is called target and the other two are distractors.\nYour task is to generate a referring expression that best describes the target image while distinguishing it from the two other distractor images",
-        "User: The first image is the target",
+        "User: What can you see in this image?",
         "images/3.png",
-        "User: The second image is a distractor",
-        "images/8.png",
-        "User: The third image is a distractor",
-        "images/13.png",
-        "User: Instruction: Describe the target image. Generate the referring expression starting with the tag 'Expression: ' for the given target image. Omit any other text",
         "<end_of_utterance>",
         "\nAssistant:",
     ],
