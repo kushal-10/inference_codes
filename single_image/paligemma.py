f5654ee88bb3aa1c31e512fc0b2e6a029685f56a
kushal-10 <kushalkoshti11@gmail.com>
2024-05-30 12:43:19 +0200

    test llava template on paligemma

--- single_image/paligemma.py
+++ single_image/paligemma.py
@@ -2,6 +2,8 @@ from transformers import AutoProcessor, AutoModelForVision2Seq, AutoConfig
 from PIL import Image
 import requests
 import torch
+from jinja2 import Template
+
 
 model_id = "google/paligemma-3b-mix-224"
 
@@ -12,35 +14,36 @@ model = AutoModelForVision2Seq.from_pretrained(model_id).eval()
 processor = AutoProcessor.from_pretrained(model_id)
 model_config = AutoConfig.from_pretrained(model_id)
 
-context = 0
-# Some models have 'max_position_embeddings' others have - 'max_sequence_length' 
-if hasattr(model_config, "text_config"):
-    context = model_config.text_config.max_position_embeddings
-    print("uses max position embeddings")
-elif hasattr(model_config, "max_sequence_length"):
-    context = model_config.max_sequence_lengths
-    print("uses max sequence length")
-
+# context = 0
+# # Some models have 'max_position_embeddings' others have - 'max_sequence_length' 
+# if hasattr(model_config, "text_config"):
+#     context = model_config.text_config.max_position_embeddings
+#     print("uses max position embeddings")
+# elif hasattr(model_config, "max_sequence_length"):
+#     context = model_config.max_sequence_lengths
+#     print("uses max sequence length")
 # print(context) #8192 context
 
 # Check chat template
-chat = [
+messages = [
   {"role": "user", "content": "Hello, how are you?"},
   {"role": "assistant", "content": "I'm doing great. How can I help you today?"},
-  {"role": "user", "content": "I'd like to show off how chat templating works!"},
+  {"role": "user", "content": "What can you see in this image?"},
 ]
 
-template_message = processor.tokenizer.apply_chat_template(chat, tokenize=False)
-print(template_message)
+# Doesn't have a chat template, because not chat-optimized, try with llava template anyway :)
+jinja_template = "{%- for message in messages -%}{% if message['role'] == 'user' %}{% if message['image'] %}\nUSER: <image>\n{{message['content']}}{% else %}\nUSER:\n{{message['content']}}{% endif %}{% elif message['role'] == 'assistant' %}\nASSISTANT:{{message['content']}}{% endif %}{% endfor %}\nASSISTANT:"
 
+temp = Template(jinja_template)
+prompt = temp.render(messages=messages)
 
 # # Instruct the model to create a caption in Spanish
 # prompt = "caption es"
-# model_inputs = processor(text=prompt, images=image, return_tensors="pt")
-# input_len = model_inputs["input_ids"].shape[-1]
+model_inputs = processor(text=prompt, images=image, return_tensors="pt")
+input_len = model_inputs["input_ids"].shape[-1]
 
-# with torch.inference_mode():
-#     generation = model.generate(**model_inputs, max_new_tokens=100, do_sample=False)
-#     generation = generation[0][input_len:]
-#     decoded = processor.decode(generation, skip_special_tokens=True)
-#     print(decoded)
+with torch.inference_mode():
+    generation = model.generate(**model_inputs, max_new_tokens=100, do_sample=False)
+    generation = generation[0][input_len:]
+    decoded = processor.decode(generation, skip_special_tokens=True)
+    print(decoded)
