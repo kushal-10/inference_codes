9fb17f90a2682499665c2f2c63d7493ccda718cb
kushal-10 <kushalkoshti11@gmail.com>
2024-06-05 20:10:32 +0200

    remove streaming code

--- single_image/llama3v25.py
+++ single_image/llama3v25.py
@@ -23,19 +23,3 @@ res = model.chat(
     # system_prompt='' # pass system_prompt if needed
 )
 print(res)
-
-## if you want to use streaming, please make sure sampling=True and stream=True
-## the model.chat will return a generator
-res = model.chat(
-    image=image,
-    msgs=msgs,
-    tokenizer=tokenizer,
-    sampling=True,
-    temperature=0.7,
-    stream=True
-)
-
-generated_text = ""
-for new_text in res:
-    generated_text += new_text
-    print(new_text, flush=True, end='')
