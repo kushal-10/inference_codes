b87a2406e9a608aaa2d76ab7ecb42b2db01c9e76
kushal-10 <kushalkoshti11@gmail.com>
2024-06-05 13:56:37 +0200

    check random token

--- additional_tests/check_api_token.py
+++ additional_tests/check_api_token.py
@@ -5,5 +5,5 @@ modelids = ["HuggingFaceM4/idefics-9b-instruct",
             "llava-hf/llava-1.5-13b-hf"]
 
 for id in modelids:
-    model_config = AutoConfig.from_pretrained(id, token="")
+    model_config = AutoConfig.from_pretrained(id, token="randomtoken")
     print(model_config)
