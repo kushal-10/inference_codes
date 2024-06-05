aba18ad66ae38935e68f756935e999d648f67b01
kushal-10 <kushalkoshti11@gmail.com>
2024-06-05 13:55:01 +0200

    check api token arg

---
+++ additional_tests/check_api_token.py
@@ -0,0 +1,9 @@
+from transformers import AutoConfig
+
+modelids = ["HuggingFaceM4/idefics-9b-instruct",
+            "llava-hf/llava-v1.6-vicuna-7b-hf",
+            "llava-hf/llava-1.5-13b-hf"]
+
+for id in modelids:
+    model_config = AutoConfig.from_pretrained(id, token=None)
+    print(model_config)
