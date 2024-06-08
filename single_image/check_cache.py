edb59993e4ed13447e8be5de5cd7da3df65792eb
kushal-10 <kushalkoshti11@gmail.com>
2024-06-08 16:13:27 +0200

    check cache

---
+++ single_image/check_cache.py
@@ -0,0 +1,13 @@
+from transformers import AutoProcessor, AutoModelForVision2Seq, AutoConfig
+from PIL import Image
+import requests
+import torch
+from jinja2 import Template
+import json
+
+
+model_id = "llava-hf/llava-1.5-13b-hf"
+
+model = AutoModelForVision2Seq.from_pretrained(model_id, cache_dir="/project/kkoshti/hf_models")
+
+print(model)
