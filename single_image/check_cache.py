165583467659c6cdd1923094c711f844e7779ce6
kushal-10 <kushalkoshti11@gmail.com>
2024-06-23 11:51:09 +0200

    test max gen for idefics

--- single_image/check_cache.py
+++ single_image/check_cache.py
@@ -5,9 +5,10 @@ import torch
 from jinja2 import Template
 import json
 
+#
+# model_id = "llava-hf/llava-1.5-13b-hf"
+# model = AutoModelForVision2Seq.from_pretrained(model_id, cache_dir="/project/kkoshti/hf_models")
+# print(model)
 
-model_id = "llava-hf/llava-1.5-13b-hf"
-
-model = AutoModelForVision2Seq.from_pretrained(model_id, cache_dir="/project/kkoshti/hf_models")
-
-print(model)
+model_id = "meta-llama/Meta-Llama-3-70B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir="/data/huggingface")
--- single_image/ide.py
+++ single_image/ide.py
@@ -44,7 +44,7 @@ prompts = [
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-checkpoint = "HuggingFaceM4/idefics-80b-instruct"
+checkpoint = "HuggingFaceM4/idefics-9b-instruct"
 processor = AutoProcessor.from_pretrained(checkpoint)
 model = IdeficsForVisionText2Text.from_pretrained(checkpoint, torch_dtype="auto", device_map="auto")
 
@@ -58,7 +58,7 @@ inputs = processor(prompts, add_end_of_utterance_token=False, return_tensors="pt
 exit_condition = processor.tokenizer("<end_of_utterance>", add_special_tokens=False).input_ids
 bad_words_ids = processor.tokenizer(["<image>", "<fake_token_around_image>"], add_special_tokens=False).input_ids
 
-generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=100)
+generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=100, max_new_tokens=100)
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
 for i, t in enumerate(generated_text):
     print(f"{i}:\n{t}\n")
