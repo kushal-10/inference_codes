04b91fd8292ec1ac594c43bad3d7b3fd0c9b7c84
kushal-10 <kushalkoshti11@gmail.com>
2024-06-23 11:54:32 +0200

    test max gen for idefics

--- single_image/ide.py
+++ single_image/ide.py
@@ -10,6 +10,18 @@ prompts = [
         "https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG",
         "<end_of_utterance>",
 
+        "User: What is in this image?",
+                "https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG",
+                "<end_of_utterance>",
+
+        "User: What is in this image?",
+                "https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG",
+                "<end_of_utterance>",
+
+        "User: What is in this image?",
+                "https://upload.wikimedia.org/wikipedia/commons/8/86/Id%C3%A9fix.JPG",
+                "<end_of_utterance>",
+
         "\nAssistant: This picture depicts Idefix, the dog of Obelix in Asterix and Obelix.<end_of_utterance>",
 
         "\nUser:",
@@ -58,7 +70,7 @@ inputs = processor(prompts, add_end_of_utterance_token=False, return_tensors="pt
 exit_condition = processor.tokenizer("<end_of_utterance>", add_special_tokens=False).input_ids
 bad_words_ids = processor.tokenizer(["<image>", "<fake_token_around_image>"], add_special_tokens=False).input_ids
 
-generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=100, max_new_tokens=100)
+generated_ids = model.generate(**inputs, eos_token_id=exit_condition, bad_words_ids=bad_words_ids, max_length=100)
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
 for i, t in enumerate(generated_text):
     print(f"{i}:\n{t}\n")
