a84968d7c8e78dd0b20d645ae310c2cf577f79d6
kushal-10 <kushalkoshti11@gmail.com>
2024-07-05 14:22:59 +0200

    test by removing history

--- single_image/intern.py
+++ single_image/intern.py
@@ -18,3 +18,10 @@ image = ['./examples/cars1.jpg',
 with torch.autocast(device_type='cuda', dtype=torch.float16):
     response, his = model.chat(tokenizer, query, image, do_sample=False, num_beams=3, use_meta=True)
 print(response)
+# To analyze the advantages and disadvantages of each car, we need to consider various factors such as performance, comfort, cost, maintenance, and brand reputation.
+
+query = 'USER: Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one ASSISTANT: To analyze the advantages and disadvantages of each car, we need to consider various factors such as performance, comfort, cost, maintenance, and brand reputation. USER: Image4 <ImageHere>; How about the car in Image4 ASSISTANT: '
+image.append('./examples/cars4.jpg')
+with torch.autocast(device_type='cuda', dtype=torch.float16):
+    response, _ = model.chat(tokenizer, query, image, do_sample=False, num_beams=3, use_meta=True)
+print(response)
