0374e584ee1ebb1bb749aaa409141815866724bd
kushal-10 <kushalkoshti11@gmail.com>
2024-07-05 14:24:28 +0200

    try with a single pass

--- single_image/intern.py
+++ single_image/intern.py
@@ -11,14 +11,14 @@ model = AutoModel.from_pretrained('internlm/internlm-xcomposer2d5-7b', torch_dty
 tokenizer = AutoTokenizer.from_pretrained('internlm/internlm-xcomposer2d5-7b', trust_remote_code=True)
 model.tokenizer = tokenizer
 
-query = 'Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one'
+# query = 'Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one'
 image = ['./examples/cars1.jpg',
         './examples/cars2.jpg',
         './examples/cars3.jpg',]
-with torch.autocast(device_type='cuda', dtype=torch.float16):
-    response, his = model.chat(tokenizer, query, image, do_sample=False, num_beams=3, use_meta=True)
-print(response)
-# To analyze the advantages and disadvantages of each car, we need to consider various factors such as performance, comfort, cost, maintenance, and brand reputation.
+# with torch.autocast(device_type='cuda', dtype=torch.float16):
+#     response, his = model.chat(tokenizer, query, image, do_sample=False, num_beams=3, use_meta=True)
+# print(response)
+# # To analyze the advantages and disadvantages of each car, we need to consider various factors such as performance, comfort, cost, maintenance, and brand reputation.
 
 query = 'USER: Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one ASSISTANT: To analyze the advantages and disadvantages of each car, we need to consider various factors such as performance, comfort, cost, maintenance, and brand reputation. USER: Image4 <ImageHere>; How about the car in Image4 ASSISTANT: '
 image.append('./examples/cars4.jpg')
