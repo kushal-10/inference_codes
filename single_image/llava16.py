b2656d6c5c58d2e209b5d4530e2343f4f3855d1c
kushal-10 <kushalkoshti11@gmail.com>
2024-07-05 12:00:18 +0200

    rm model shift

--- single_image/llava16.py
+++ single_image/llava16.py
@@ -78,14 +78,15 @@ import requests
 processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-34b-hf")
 
 model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-34b-hf", torch_dtype=torch.float16, low_cpu_mem_usage=True)
-model.to("cuda:0")
+# model.to("cuda:0")
 
 # prepare image and text prompt, using the appropriate prompt template
 url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
 image = Image.open(requests.get(url, stream=True).raw)
 prompt = "<|im_start|>system\nAnswer the questions.<|im_end|><|im_start|>user\n<image>\nWhat is shown in this image?<|im_end|><|im_start|>assistant\n"
 
-inputs = processor(prompt, image, return_tensors="pt").to("cuda:0")
+# inputs = processor(prompt, image, return_tensors="pt").to("cuda:0")
+inputs = processor(prompt, image, return_tensors="pt")
 
 # autoregressively complete prompt
 output = model.generate(**inputs, max_new_tokens=100)
