f6bc9e43c0adb4382f3bdfe5a27dae0d28b311af
kushal-10 <kushalkoshti11@gmail.com>
2024-07-05 13:19:34 +0200

    rm devices, set torch_dtype to auto

--- single_image/intern.py
+++ single_image/intern.py
@@ -7,15 +7,14 @@ from transformers import AutoModel, AutoTokenizer
 torch.set_grad_enabled(False)
 
 # init model and tokenizer
-model = AutoModel.from_pretrained('internlm/internlm-xcomposer2d5-7b', torch_dtype=torch.bfloat16, trust_remote_code=True, device_map='auto').cuda().eval()
+model = AutoModel.from_pretrained('internlm/internlm-xcomposer2d5-7b', torch_dtype='auto', trust_remote_code=True, device_map='auto')
 tokenizer = AutoTokenizer.from_pretrained('internlm/internlm-xcomposer2d5-7b', trust_remote_code=True)
 model.tokenizer = tokenizer
 
-query = 'Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; Image4 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one'
+query = 'Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one'
 image = ['./examples/cars1.jpg',
          './examples/cars2.jpg',
-         './examples/cars3.jpg',
-         './examples/cars4.jpg',]
-with torch.autocast(device_type='cuda', dtype=torch.float16):
+         './examples/cars3.jpg']
+with torch.autocast(device_type='cuda', dtype='auto'):
     response, his = model.chat(tokenizer, query, image, do_sample=True, num_beams=3, use_meta=True)
 print(response)
