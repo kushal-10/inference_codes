f26ca94f497316d0a1d24ecbc30de8efa46e29f6
kushal-10 <kushalkoshti11@gmail.com>
2024-07-05 13:11:03 +0200

    test w 4 images

--- single_image/intern.py
+++ single_image/intern.py
@@ -9,12 +9,14 @@ torch.set_grad_enabled(False)
 # init model and tokenizer
 model = AutoModel.from_pretrained('internlm/internlm-xcomposer2d5-7b', torch_dtype=torch.bfloat16, trust_remote_code=True).cuda().eval()
 tokenizer = AutoTokenizer.from_pretrained('internlm/internlm-xcomposer2d5-7b', trust_remote_code=True)
+processor = AutoProcessor.from_pretrained('internlm/internlm-xcomposer2d5-7b', )
 model.tokenizer = tokenizer
 
-query = 'Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one'
+query = 'Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; Image4 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one'
 image = ['./examples/cars1.jpg',
-        './examples/cars2.jpg',
-        './examples/cars3.jpg',]
+         './examples/cars2.jpg',
+         './examples/cars3.jpg',
+         './examples/cars4.jpg',]
 with torch.autocast(device_type='cuda', dtype=torch.float16):
     response, his = model.chat(tokenizer, query, image, do_sample=True, num_beams=3, use_meta=True)
 print(response)
