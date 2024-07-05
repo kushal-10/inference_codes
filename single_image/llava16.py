410e7b6e8e9569d110d7a9f0a9ab4ff71868773e
kushal-10 <kushalkoshti11@gmail.com>
2024-07-05 12:02:24 +0200

    shift inputs to cuda

--- single_image/llava16.py
+++ single_image/llava16.py
@@ -77,8 +77,8 @@ import requests
 
 processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-34b-hf")
 
-model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-34b-hf", torch_dtype=torch.float16, low_cpu_mem_usage=True)
-# model.to("cuda:0")
+model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-34b-hf")
+model.to("cuda")
 
 # prepare image and text prompt, using the appropriate prompt template
 url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
