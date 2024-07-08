195c132676c13fbae381420c2cbbf8aed1f56904
kushal-10 <kushalkoshti11@gmail.com>
2024-07-08 19:16:24 +0200

    add dev map auto

--- single_image/intern.py
+++ single_image/intern.py
@@ -7,8 +7,8 @@ from transformers import AutoModel, AutoTokenizer
 torch.set_grad_enabled(False)
 
 # init model and tokenizer
-model = AutoModel.from_pretrained('internlm/internlm-xcomposer2d5-7b', torch_dtype=torch.bfloat16, trust_remote_code=True).cuda().eval()
-tokenizer = AutoTokenizer.from_pretrained('internlm/internlm-xcomposer2d5-7b', trust_remote_code=True)
+model = AutoModel.from_pretrained('internlm/internlm-xcomposer2d5-7b', torch_dtype=torch.bfloat16, trust_remote_code=True, device_map='auto')
+tokenizer = AutoTokenizer.from_pretrained('internlm/internlm-xcomposer2d5-7b', trust_remote_code=True, device_map='auto')
 model.tokenizer = tokenizer
 
 # query = 'Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one'
@@ -23,5 +23,5 @@ image = ['./examples/cars1.jpg',
 query = 'USER: Image1 <ImageHere>; Image2 <ImageHere>; Image3 <ImageHere>; I want to buy a car from the three given cars, analyze their advantages and weaknesses one by one ASSISTANT: To analyze the advantages and disadvantages of each car, we need to consider various factors such as performance, comfort, cost, maintenance, and brand reputation. USER: Image4 <ImageHere>; How about the car in Image4 ASSISTANT: '
 image.append('./examples/cars4.jpg')
 with torch.autocast(device_type='cuda', dtype=torch.float16):
-    response, _ = model.chat(tokenizer, query, image, do_sample=False, num_beams=3, use_meta=True)
+    response, _ = model.chat(tokenizer, query, image)
 print(response)
